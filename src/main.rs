630aef47d75834a2597634e479ea2268c80e5af8
prajnastra <coder.abhijit90@gmail.com>
2023-04-01 17:44:50 +0530

    add: clap arg parser & iamge crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+use image::image_dimensions;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let (w, h) = image_dimensions(args.path).expect("Not able to read");
+
+    println!("Dimensions {}x{}", w, h);
 }
